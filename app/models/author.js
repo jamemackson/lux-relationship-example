b7b3fb7f82ac2e7959b5499f2db95eee3b007825
jame mackson <jame.mackson@gmail.com>
2017-02-02 10:08:43 -0600

    update: add relationships and update db data

--- app/models/author.js
+++ app/models/author.js
@@ -1,7 +1,13 @@
 import { Model } from 'lux-framework';
 
 class Author extends Model {
-
+  static hasMany = {
+    books: {
+      inverse: 'author'
+      // The line above lets Lux know that this relationship is accessible
+      // on book instances via `book.author`.
+    }
+  };
 }
 
 export default Author;
--- app/models/book.js
+++ app/models/book.js
@@ -1,7 +1,14 @@
 import { Model } from 'lux-framework';
 
 class Book extends Model {
-
+  static belongsTo = {
+    author: {
+      inverse: 'books',
+      // The line above lets Lux know that this relationship is accessible
+      // on author instances via `author.books`.
+      foreignKey: 'author_id'
+    }
+  };
 }
 
 export default Book;
